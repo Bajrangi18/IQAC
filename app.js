4d519d65473a70540e26b533a5ba5d1eed8dc751
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-05-10 23:49:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 document.getElementById("naac_button").addEventListener("click", myFunction);
 
 function myFunction() {
-    document.getElementById("naac_stuff").display = "inline";
+    document.getElementById("naac_stuff").style.display = "block";
+}
+
+document.getElementById("iqac_button").addEventListener("click", myFunction2);
+
+function myFunction2() {
+    document.getElementById("naac_stuff").style.display = "none";
 }
