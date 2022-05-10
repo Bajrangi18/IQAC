1093a7da7f640c45e1114911a457f2f73962cfd0
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-05-10 23:25:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-smoothScroll.init();
+document.getElementById("naac_button").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("naac_stuff").display = "inline";
+}
