6950824364ea4acf5474d2976d3242d8dc9323d5
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-05-11 00:14:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+window.addEventListener('load', (event) => {
+   document.getElementById("naac_stuff").style.display = "none";
+});
+
 document.getElementById("naac_button").addEventListener("click", myFunction);
 
 function myFunction() {
     document.getElementById("naac_stuff").style.display = "block";
-    document.getElementById("iqac_button").style.display = "block";
-    document.getElementById("iqac_name").style.display = "block";
 }
 
 document.getElementById("iqac_button").addEventListener("click", myFunction2);
 
 function myFunction2() {
     document.getElementById("naac_stuff").style.display = "none";
-    document.getElementById("iqac_button").style.display = "none";
-    document.getElementById("iqac_name").style.display = "none";
 }
